1f35c1608b27574169f4b5723c8b9e6e75d43f27
Dean Lofts <dean@deanlofts.xyz>
2023-05-28 20:54:30 +1000

    remove ifs

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,18 +1,23 @@
 class FizzBuzz
-    def initialize(start, finish)
-      @start = start
-      @finish = finish
-    end
-  
-    def call
-      (@start..@finish).each do |i|
-        output = ''
-        output << 'Fizz' if i % 3 == 0
-        output << 'Buzz' if i % 5 == 0
-        puts output.empty? ? i : output
-      end
+  def initialize(start, finish)
+    @start = start
+    @finish = finish
+    @responses = {
+      0 => proc { |i| 'FizzBuzz' },
+      3 => proc { |i| 'Fizz' },
+      5 => proc { |i| 'Buzz' },
+      6 => proc { |i| 'Fizz' },
+      9 => proc { |i| 'Fizz' },
+      10 => proc { |i| 'Buzz' }
+    }
+  end
+
+  def call
+    (@start..@finish).each do |i|
+      output = @responses[i % 15].call(i) rescue i
+      puts output
     end
   end
-  
-  FizzBuzz.new(1, 100).call
-  
+end
+
+FizzBuzz.new(1, 100).call
