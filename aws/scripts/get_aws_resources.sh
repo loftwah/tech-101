214ee9308b24560b735b3262fd7b13c8efd31197
Dean Lofts <dean@deanlofts.xyz>
2023-07-25 16:29:03 +1000

    output to json

--- aws/scripts/get_aws_resources.sh
+++ aws/scripts/get_aws_resources.sh
@@ -2,38 +2,40 @@
 
 REGIONS=("ap-southeast-2" "us-west-1" "eu-west-1" "us-east-1")
 PROFILE=${1:-"default"}  # Default to "default" if no argument is provided
-OUTPUT_FILE=${2:-"aws_resources.txt"}  # Default to "aws_resources.txt" if no argument is provided
-
-# Clear the output file at the start
-echo "" > $OUTPUT_FILE
 
 for region in "${REGIONS[@]}"; do
-    echo "Region: $region" | tee -a $OUTPUT_FILE
+    echo "Processing region: $region"
 
-    echo "EC2 instances:" | tee -a $OUTPUT_FILE
-    aws ec2 describe-instances --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "EC2 instances:"
+    aws ec2 describe-instances --query 'Reservations[].Instances[].{InstanceId:InstanceId, InstanceType:InstanceType, State:State.Name}' --output json --region $region --profile $PROFILE > "EC2_${region}.json"
 
-    echo "S3 buckets:" | tee -a $OUTPUT_FILE
-    aws s3api list-buckets --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "S3 buckets:"
+    aws s3api list-buckets --query 'Buckets[].Name' --output json --region $region --profile $PROFILE > "S3_${region}.json"
 
-    echo "ECS clusters:" | tee -a $OUTPUT_FILE
-    aws ecs list-clusters --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "ECS clusters:"
+    aws ecs list-clusters --query 'clusterArns[]' --output json --region $region --profile $PROFILE > "ECS_${region}.json"
 
-    echo "ECR repositories:" | tee -a $OUTPUT_FILE
-    aws ecr describe-repositories --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "ECR repositories:"
+    aws ecr describe-repositories --query 'repositories[].repositoryName' --output json --region $region --profile $PROFILE > "ECR_${region}.json"
 
-    echo "SNS topics:" | tee -a $OUTPUT_FILE
-    aws sns list-topics --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "SNS topics:"
+    aws sns list-topics --query 'Topics[].TopicArn' --output json --region $region --profile $PROFILE > "SNS_${region}.json"
 
-    echo "SQS queues:" | tee -a $OUTPUT_FILE
-    aws sqs list-queues --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "SQS queues:"
+    aws sqs list-queues --query 'QueueUrls[]' --output json --region $region --profile $PROFILE > "SQS_${region}.json"
 
-    echo "RDS instances:" | tee -a $OUTPUT_FILE
-    aws rds describe-db-instances --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "RDS instances:"
+    aws rds describe-db-instances --query 'DBInstances[].DBInstanceIdentifier' --output json --region $region --profile $PROFILE > "RDS_${region}.json"
 
-    echo "ElastiCache clusters:" | tee -a $OUTPUT_FILE
-    aws elasticache describe-cache-clusters --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "ElastiCache clusters:"
+    aws elasticache describe-cache-clusters --query 'CacheClusters[].CacheClusterId' --output json --region $region --profile $PROFILE > "ElastiCache_${region}.json"
 
-    echo "SES identities:" | tee -a $OUTPUT_FILE
-    aws ses list-identities --region $region --profile $PROFILE >> $OUTPUT_FILE
+    echo "SES identities:"
+    aws ses list-identities --query 'Identities[]' --output json --region $region --profile $PROFILE > "SES_${region}.json"
+
+    echo "ELBv2 load balancers:"
+    aws elbv2 describe-load-balancers --query 'LoadBalancers[].LoadBalancerName' --output json --region $region --profile $PROFILE > "ELBv2_${region}.json"
+
+    echo "ELBv2 target groups:"
+    aws elbv2 describe-target-groups --query 'TargetGroups[].TargetGroupName' --output json --region $region --profile $PROFILE > "TargetGroups_${region}.json"
 done
