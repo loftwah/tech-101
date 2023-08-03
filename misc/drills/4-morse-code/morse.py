eebe5876263279ef29ebf6c0742255fe274e6e47
Dean Lofts <dean@deanlofts.xyz>
2023-08-03 22:54:50 +1000

    support custom words

--- misc/drills/4-morse-code/morse.py
+++ misc/drills/4-morse-code/morse.py
@@ -1,16 +1,26 @@
 from manim import *
 
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..'}
+
 class MorseCode(Scene):
     def construct(self):
-        code = {
-            "D": "- . .",
-            "E": ".",
-            "Z": "- - . .",
-            "N": "- .",
-            "U": ". . -",
-            "T": "-",
-        }
-        for letter, morse in code.items():
+        # Read word from file
+        with open('word.txt', 'r') as file:
+            word = file.read().strip().upper()
+
+        # Convert word to Morse code using the MORSE_CODE_DICT
+        code = [(letter, MORSE_CODE_DICT[letter]) for letter in word if letter in MORSE_CODE_DICT]
+
+        # Animate each letter
+        for letter, morse in code:
             self.play_flash(letter, morse)
 
     def play_flash(self, letter, morse):
@@ -30,15 +40,15 @@ class MorseCode(Scene):
         self.wait(2)
 
     def play_flash_dot(self):
-        circle = Circle(fill_opacity=1).scale(2)
+        circle = Circle(fill_opacity=1).scale(1)
         self.play(FadeIn(circle))
-        self.wait(0.5)
+        self.wait(0.3)
         self.play(FadeOut(circle))
-        self.wait(0.5)
+        self.wait(0.3)
 
     def play_flash_dash(self):
-        circle = Circle(fill_opacity=1).scale(2)
+        circle = Circle(fill_opacity=1).scale(1)
         self.play(FadeIn(circle))
-        self.wait(1.5)
+        self.wait(0.6)
         self.play(FadeOut(circle))
-        self.wait(0.5)
+        self.wait(0.3)
