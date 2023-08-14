1ed6ef4383f391103244a774c724ac9414a00b07
Dean Lofts <dean@deanlofts.xyz>
2023-08-14 23:05:49 +1000

    support dcma repo

--- ruby/github_repo_delete.rb
+++ ruby/github_repo_delete.rb
@@ -1,10 +1,10 @@
 require 'octokit'
 
 # Replace this with your GitHub personal access token
-ACCESS_TOKEN = '<your-token>'
+ACCESS_TOKEN = '<your-access-token>'
 
 # GitHub username
-USERNAME = '<your-username>'
+USERNAME = 'loftwah'
 
 # Set to true for a dry run (no actual deletions)
 DRY_RUN = false
@@ -23,22 +23,28 @@ loop do
   break if repos.empty?
 
   # Iterate through the repositories on the current page
-  repos.each do |repo|
-    # Get the updated_at date (already a Time object)
-    updated_at = repo.updated_at
+repos.each do |repo|
+  # Get the updated_at date (already a Time object)
+  updated_at = repo.updated_at
 
-    # Check if the repository was updated before 2021
-    if updated_at.year < 2021
-      if DRY_RUN
-        # Print the repository name without deleting (dry run)
-        puts "Would delete repository: #{repo.full_name}"
-      else
+  # Check if the repository was updated before 2021
+  if updated_at.year < 2022
+    if DRY_RUN
+      # Print the repository name without deleting (dry run)
+      puts "Would delete repository: #{repo.full_name}"
+    else
+      begin # Adding 'begin' here
         # Delete the repository if not updated in 2023
         client.delete_repo(repo.full_name)
         puts "Deleted repository: #{repo.full_name}"
-      end
+      rescue Octokit::UnavailableForLegalReasons
+        puts "Repository access blocked, cannot delete: #{repo.full_name}"
+      rescue => e
+        puts "Error deleting repository: #{repo.full_name}. Error: #{e.message}"
+      end # Closing 'end' here
     end
   end
+end
 
   # Increment the page number for the next iteration
   page_number += 1
